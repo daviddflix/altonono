eaa7b3db711cc0b3ca6f24b9b901f71b40b357a7
davidflix <david-972010@hotmail.com>
2022-09-14 12:03:45 -0300

    change port

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -14,7 +14,7 @@ export const CLEAR_DETAIL = 'CLEAR_DETAIL'
 
 
 
-const url= 'https://altonono.herokuapp.com/'
+const url= 'https://altonono.herokuapp.com'
 
 export function getProduct (){   
        return async function (dispatch){
